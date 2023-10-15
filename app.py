cf302ff561db860222fbce3054a4cda5c22f0b16
MaruthiKo <marurohi@gmail.com>
2023-10-15 23:29:54 +0530

    Webpage Completed

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-def predict():
+def predict_api():
     data = request.json["data"]
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -21,6 +21,15 @@ def predict():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The predicted house price is {}".format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
